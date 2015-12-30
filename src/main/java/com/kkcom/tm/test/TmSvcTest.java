6b5a8e74a82f2f72ec34e761d280bd5c4a7e8c9a
knkhattar <knkhattar@gmail.com>
2015-12-30 14:56:21 +0530

    Initial version

---
+++ src/main/java/com/kkcom/tm/test/TmSvcTest.java
@@ -0,0 +1,19 @@
+package com.kkcom.tm.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.kkcom.tm.login.svc.AuthService;
+
+public class TmSvcTest {
+	
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"spring-context.xml");
+
+		AuthService authService = (AuthService) context.getBean("authService");
+		
+		System.out.println("auth status ::"+authService.authenticate("testuser", "testpass").isAuthenticated());
+	}
+}
