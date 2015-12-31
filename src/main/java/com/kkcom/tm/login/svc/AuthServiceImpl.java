99ce1be4c7a3cc2753eb4e2b04858c633eba1c21
knkhattar <knkhattar@gmail.com>
2015-12-31 18:44:57 +0530

    Updated to include log4j logging

--- src/main/java/com/kkcom/tm/login/svc/AuthServiceImpl.java
+++ src/main/java/com/kkcom/tm/login/svc/AuthServiceImpl.java
@@ -2,6 +2,7 @@ package com.kkcom.tm.login.svc;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -13,7 +14,8 @@ import com.kkcom.tm.login.model.Users;
 @Service
 public class AuthServiceImpl implements AuthService {
 
-	
+	static Logger logger = Logger.getLogger(AuthServiceImpl.class);
+
 	private SessionFactory sessionFactory;
 
 	public SessionFactory getSessionFactory() {
@@ -23,15 +25,18 @@ public class AuthServiceImpl implements AuthService {
 	public void setSessionFactory(SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
 	}
-	
+
 	public AuthStatus authenticate(String username, String password) {
+		logger.info("Inside authenticate method");
 
 		AuthStatus authStatus = new AuthStatus();
 		authStatus.setAuthenticated(checkDbForAuth(username, password));
 
+		logger.info("Exit authenticate method, auth status::" + authStatus);
 		return authStatus;
 	}
 
+	@SuppressWarnings("unchecked")
 	private boolean checkDbForAuth(String userName, String password) {
 		boolean isAuthenticated = false;
 		Session session = null;
@@ -40,21 +45,20 @@ public class AuthServiceImpl implements AuthService {
 			if (session == null) {
 				System.out.println("dataSource is null ::");
 			}
-			
+
 			String hql = "from Users where name = :userName and password = :password";
 			Query query = session.createQuery(hql);
 			query.setString("userName", userName);
 			query.setString("password", password);
-			List result = query.list();
-			if(result.size() >= 1)
-			{
+			List<Users> result = query.list();
+			if (result.size() >= 1) {
 				isAuthenticated = true;
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return isAuthenticated;
 	}
 
